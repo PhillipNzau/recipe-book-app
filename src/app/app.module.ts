5e8f13c0d16711a0ed3ab2506d1a5fb62abf851b
phillip nzau junior <nzau78@gmail.com>
2021-01-25 16:19:08 +0300

    new feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppinglistComponent } from './components/shoppinglist/shoppinglist.component';
+import { RecipebookComponent } from './components/recipebook/recipebook.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from './components/recipebook/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './components/recipebook/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './components/recipebook/recipe-list/recipe-item/recipe-item.component';
+import { ShoppinglistEditComponent } from './components/shoppinglist/shoppinglist-edit/shoppinglist-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppinglistComponent,
+    RecipebookComponent,
+    HeaderComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppinglistEditComponent
   ],
   imports: [
     BrowserModule,
